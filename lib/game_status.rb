5ddf1b2d79fb84d993b059e588ee30d2b1000a7a
Rach <yellatme740@hotmail.com>
2017-10-20 14:36:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,8 +27,10 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    else position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
+    else
+      return false
     end
   end
 end
