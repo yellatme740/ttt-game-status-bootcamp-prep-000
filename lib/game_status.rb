3838176070d5d4f7b9ff43aca61c96b98cc74ca0
Rach <yellatme740@hotmail.com>
2017-10-20 14:41:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,3 +35,14 @@ def won?(board)
   end
   return false
 end
+
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |combo|
+#     board[combo[0]] == board[combo[1]] &&
+#     board[combo[1]] == board[combo[2]] &&
+#     position_taken?(board, combo[0])
+#   end
+# end
+
+def full?
+end
